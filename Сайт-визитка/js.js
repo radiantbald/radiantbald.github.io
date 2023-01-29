d8ad3075491bb17e171785f45c9466ce895ffe98
OLEG POPOV <95772173+radiantbald@users.noreply.github.com>
2023-01-29 12:17:20 +0300

    Delete Сайт-визитка directory

--- Сайт-визитка/js.js
+++
@@ -1,7 +0,0 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
-
-/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert('быть лучше, чем вчера')
-})
